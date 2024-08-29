9fd1d4e813c638d301320ed70390f8afa93959ca
Ankit <iankitraj18@gmail.com>
2024-08-29 22:25:50 +0530

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(Center(
-    child: Text(
-      'Hello Ankit!', textDirection: TextDirection.ltr,
+  runApp(
+    MaterialApp(
+      title: 'My flutter App',
+      home: Center(
+        child: Text(
+          'Hello, Ankit!',
+          textDirection: TextDirection.ltr,
+          ),
       ),
-      ),
+    )
       );
 }
