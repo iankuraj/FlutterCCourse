ee09aef8b139bad7d204001c97298493772ef6a5
Ankit <iankitraj18@gmail.com>
2024-08-29 23:07:13 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,26 @@ void main(){
   runApp(
     MaterialApp(
       title: 'My flutter App',
-      home: Center(
-        child: Text(
-          'Hello, Ankit!',
-          textDirection: TextDirection.ltr,
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'.toUpperCase()),),
+          backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Text.rich(
+            TextSpan(text: 'My',
+            children: [
+              TextSpan(
+                text: 'Flutter',
+                style: 
+                TextStyle(fontSize: 50.0, fontWeight:FontWeight.bold)),
+              TextSpan(
+                text: 'App',
+                style: TextStyle(fontSize: 30.0,color: Colors.blue)),
+            ],
+            ),
+        ),
       ),
-    )
-      );
+    ),
+    ),
+    );
 }
